794c467885f49dbc34e5a5c9d365cd087889080a
Alec Edgington <alec.edgington@quantinuum.com>
2023-11-20 15:56:11 +0000

    Add support for error models and RNG seeds.

--- src/pytket_pecos/emulator.py
+++ src/pytket_pecos/emulator.py
@@ -1,12 +1,20 @@
 from pecos.engines.hybrid_engine import HybridEngine
+from pecos.error_models.error_model_abc import ErrorModel
 from pytket.circuit import Circuit
 from pytket.phir.api import pytket_to_phir
+from typing import Optional
 
 
 class Emulator:
-    def __init__(self, circuit: Circuit):
+    def __init__(
+        self,
+        circuit: Circuit,
+        error_model: Optional[ErrorModel] = None,
+        seed: Optional[int] = None,
+    ):
         self.phir = pytket_to_phir(circuit)
-        self.engine = HybridEngine()
+        self.engine = HybridEngine(error_model=error_model)
+        self.engine.use_seed(seed)
 
     def run(self, n_shots):
         return self.engine.run(self.phir, shots=n_shots)
--- test/test_emulator.py
+++ test/test_emulator.py
@@ -1,5 +1,6 @@
 import unittest
 
+from pecos.error_models.generic_error_model import GenericErrorModel
 from pytket.circuit import Circuit
 from pytket_pecos import Emulator
 
@@ -14,6 +15,30 @@ class TestEmulator(unittest.TestCase):
         vals = results["c"]
         self.assertTrue(len(vals) == n_shots)
         self.assertTrue(all(c0 == c1 for (c0, c1) in vals))
+
+    def test_bell_with_noise(self):
+        c = Circuit(2).H(0).CX(0, 1).measure_all()
+        error_model = GenericErrorModel(
+            error_params={
+                "p1": 2e-1,
+                "p2": 2e-1,
+                "p_meas": 2e-1,
+                "p_init": 1e-1,
+                "p1_error_model": {
+                    "X": 0.25,
+                    "Y": 0.25,
+                    "Z": 0.25,
+                    "L": 0.25,
+                },
+            },
+        )
+        emu = Emulator(c, error_model=error_model, seed=7)
+        n_shots = 100
+        results = emu.run(n_shots=n_shots)
+        self.assertTrue(list(results.keys()) == ["c"])
+        vals = results["c"]
+        self.assertTrue(len(vals) == n_shots)
+        self.assertTrue(len([(c0, c1) for (c0, c1) in vals if c0 == c1]) == 88)
 
 
 if __name__ == "__main__":
